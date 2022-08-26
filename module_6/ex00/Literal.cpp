1fb1eb1976c1250ae2dc0efbde89aae4bfd38a89
Pierre Saulnier <psaulnie@z2r9p2.42lyon.fr>
2022-08-26 18:55:31 +0200

    wip ex00 cpp06

---
+++ module_6/ex00/Literal.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Literal.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaulnie <psaulnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 14:37:52 by psaulnie          #+#    #+#             */
+/*   Updated: 2022/08/25 15:46:42 by psaulnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Literal.hpp"
+
+Literal::Literal(std::string value)
+{
+	this->value = value;
+}
+
+Literal::Literal(Literal &copy)
+{
+	this->value = copy.value;
+}
+
+Literal	&Literal::operator=(Literal &copy)
+{
+	this->value = copy.value;
+	return (*this);
+}
+
+
+Literal::operator	int()
+{
+	char	char_val;
+	int		val;
+	if (this->value.size() == 1 && (this->value.c_str()[0] > 57 || this->value.c_str()[0] < 48))
+	{
+		char_val = this->value.c_str()[0];
+		val = char_val;
+	}
+	else
+		val = std::atoi(this->value.c_str());
+	return (val);
+}
+
+Literal::operator	float()
+{
+	return (static_cast<float>(std::atof(this->value.c_str())));
+}
+
+Literal::operator	double()
+{
+	return (std::atof(this->value.c_str()));
+}
+
+Literal::operator	char()
+{
+	char	val;
+	if (this->value.size() == 1 && (this->value.c_str()[0] > 57 || this->value.c_str()[0] < 48))
+		val = this->value.c_str()[0];
+	else
+		val = std::atoi(this->value.c_str());
+	if (val < 0 || val > 127)
+		return (-1);
+	else if (val < 32)
+		return (-2);
+	return (val);
+}
+
+Literal::~Literal()
+{
+}
---
+++ module_6/ex00/Literal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Literal.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaulnie <psaulnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 12:10:52 by psaulnie          #+#    #+#             */
+/*   Updated: 2022/08/25 15:00:03 by psaulnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LITERAL_HPP
+# define	LITERAL_HPP
+
+#include <iostream>
+
+class Literal
+{
+private:
+	std::string	value;
+public:
+	Literal(std::string value);
+	Literal(Literal &copy);
+	Literal	&operator=(Literal &copy);
+	~Literal();
+
+	operator	int();
+	operator	float();
+	operator	double();
+	operator	char();
+};
+
+#endif
