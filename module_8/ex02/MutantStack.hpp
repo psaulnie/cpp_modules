af76c5123ee9dee4e53e0072daa02d12d656eaae
Pierrot <pierrot@localhost.localdomain>
2022-09-01 11:28:26 +0200

    cpp08 finished

--- module_8/ex02/MutantStack.hpp
+++ module_8/ex02/MutantStack.hpp
@@ -6,31 +6,29 @@
 /*   By: psaulnie <psaulnie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 15:22:40 by psaulnie          #+#    #+#             */
-/*   Updated: 2022/08/31 16:06:46 by psaulnie         ###   ########.fr       */
+/*   Updated: 2022/09/01 11:10:15 by psaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+#include <stack>
+#include <iostream>
+
 template<typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
+		typedef typename	std::stack<T>::container_type::const_iterator	iterator;
 		MutantStack();
-		MutantStack(MutantStack const &copy);
-		MutantStack	&operator=(MutantStack const &copy);
-		virtual ~MutantStack();
+		MutantStack(MutantStack<T> const &copy);
+		MutantStack	&operator=(MutantStack<T> const &copy);
+		~MutantStack() {};
 
-	// class iterator
-	// {
-	// 	private:
-	// 		/* data */
-	// 	public:
-	// 		MutantStack(/* args */);
-	// 		~MutantStack();
-	// };
-	
+		iterator	begin() const { return (std::stack<T>::c.begin()); };
+		iterator	end() const { return (std::stack<T>::c.end()); };
+
 };
 
 #endif
--- module_8/ex02/MutantStack.tpp
+++ module_8/ex02/MutantStack.tpp
@@ -6,26 +6,21 @@
 /*   By: psaulnie <psaulnie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 15:23:39 by psaulnie          #+#    #+#             */
-/*   Updated: 2022/08/31 16:05:50 by psaulnie         ###   ########.fr       */
+/*   Updated: 2022/09/01 11:10:42 by psaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
 template<typename T>
-MutantStack::MutantStack()
-{
-	
-}
+MutantStack<T>::MutantStack() : std::stack<T>() { }
 
 template<typename T>
-MutantStack::MutantStack(MutantStack<T> const &copy)
-{
-	
-}
+MutantStack<T>::MutantStack(MutantStack<T> const &copy) : std::stack<T>(copy) { }
 
 template<typename T>
-MutantStack<T>	MutantStack::operator=(MutantStack<T> const &copy)
+MutantStack<T>	&MutantStack<T>::operator=(MutantStack<T> const &copy)
 {
-	
+	*this = copy;
+	return (*this);
 }
